557381e08d292a763bfcc0bee9360fb5407c8cee
Owen Nyo <94278623+OwenNyo@users.noreply.github.com>
2025-05-27 17:59:52 +0800

    Setup DB + amended Login page

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,17 +2,30 @@ from flask import Flask
 from flask_cors import CORS
 from .db import db
 import os
-from .routes.credits import credits_bp
+from .routes.credits_bp import credits_bp
+from .routes.users_bp import users_bp
+from sqlalchemy import text
 
 # Initialise app
 def create_app():
     app = Flask(__name__)
-    #CORS to communicate with frontend
+    # CORS for frontend
     CORS(app, resources={r"/api/*": {"origins": {os.getenv("FRONTEND_ROUTE")}}})
-    
+
+    # Connect to db
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('SQLALCHEMY_DATABASE_URI')
     db.init_app(app)
-
+    
+    # Register blueprints
     app.register_blueprint(credits_bp, url_prefix='/api')
+    app.register_blueprint(users_bp, url_prefix='/api')
+
+    # Test database connection
+    with app.app_context():
+        try:
+            db.session.execute(text("SELECT 1"))
+            print("✅ Successfully connected to the database.")
+        except Exception as e:
+            print("❌ Failed to connect to the database:", e)
 
     return app
--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -1,19 +1,18 @@
 from app.db import db
-from sqlalchemy.dialects.mysql import ENUM
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'Users'
+    __table_args__ = {'schema': 'dbo'}
 
-    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    profile_picture = db.Column(db.String(255), default='', nullable=True)
-    membership = db.Column(ENUM('basic', 'premium'), default='basic')
-    created_at = db.Column(db.DateTime, server_default=db.func.current_timestamp())
-    last_login = db.Column(db.DateTime, server_default=db.func.current_timestamp())
-    post_limit = db.Column(db.Integer, default=2)
+    userID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userName = db.Column(db.String(255), nullable=False)
+    mobileNumber = db.Column(db.String(20), nullable=True)
+    Role = db.Column(db.String(10), nullable=False)
 
-
-
-    # JUST AN EXAMPLE THIS MY SSD SETUP WE USE SQLALCHEMY TO TALK TO MYSQL AS A ORM
+    def to_dict(self):
+        return {
+            "userID": self.userID,
+            "userName": self.userName,
+            "mobileNumber": self.mobileNumber,
+            "Role": self.Role
+        }
