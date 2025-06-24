75491f657ed82f2d08fa6af0571c21c91b04c9f9
leonseehaojun <leonsee1000@yahoo.com.sg>
2025-06-24 14:59:17 +0800

    adding of sutdents referencing student table

---
+++ backend/app/models/students.py
@@ -0,0 +1,10 @@
+# app/models/students.py
+from app.db import db
+
+class Student(db.Model):
+    __tablename__ = 'Students'
+
+    studentID = db.Column(db.String(10), primary_key=True)
+    fullName = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    mobileNumber = db.Column(db.String(20), nullable=True)
