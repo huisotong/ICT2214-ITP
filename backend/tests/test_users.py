1b79e01fcbfa69a99c0da4206330d0b252abdffb
OwenNyo <2301840@sit.singaporetech.edu.sg>
2025-09-18 15:31:24 +0800

    Update test_users.py

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -1,15 +1,6 @@
 
 import json
 
-def test_get_all_users(test_client):
-    """
-    GIVEN a Flask application configured for testing
-    WHEN the '/api/users' page is requested (GET)
-    THEN check that the response is valid
-    """
-    response = test_client.get('/api/users')
-    assert response.status_code == 200
-
 def test_login(test_client):
     """
     GIVEN a Flask application configured for testing
